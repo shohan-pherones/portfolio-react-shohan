17d221c32b2cfe281808762dcf1acf5439b01eef
shohan-pherones <shohan.sub.56@gmail.com>
2022-10-22 02:01:09 +0600

    hero added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,14 @@ import Skills from "./Skills";
 import About from "./About";
 import Contact from "./Contact";
 import Projects from "./Projects";
+import Hero from "./Hero";
 
 const Home = () => {
   const height = "h-auto";
 
   return (
     <>
+      <Hero />
       <Projects bypassHeight={height} />
       <Skills bypassHeight={height} />
       <About bypassHeight={height} />
